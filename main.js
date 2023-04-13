5da71e4f52fed1698ae8b0fc947a605256e7e0ea
viennap <22viennap@gmail.com>
2023-04-13 14:32:30 -0500

    Fixed?

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
 const button = document.getElementById('click-counter-btn');
 const clickCounter = document.getElementById('click-counter');
 
-let clickCount = getClickCountFromCookie();
+let clickCount = parseInt(localStorage.getItem('clickCount')) || 0;
+clickCounter.innerText = clickCount;
 
 button.addEventListener('click', () => {
   clickCount++;
   clickCounter.innerText = clickCount;
-  setClickCountCookie(clickCount);
+  localStorage.setItem('clickCount', clickCount.toString());
 });
-
-function getClickCountFromCookie() {
-  const name = 'clickCount=';
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) == ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(name) == 0) {
-      return parseInt(cookie.substring(name.length, cookie.length));
-    }
-  }
-  return 0;
-}
-
-function setClickCountCookie(clickCount) {
-  const date = new Date();
-  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-  const expires = 'expires=' + date.toUTCString();
-  document.cookie = 'clickCount=' + clickCount + ';' + expires + ';path=/';
-}
