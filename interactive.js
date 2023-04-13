f170ff9d1a54d36841cf08d98cfc380370e2005f
viennap <22viennap@gmail.com>
2023-04-13 14:00:08 -0500

    Update

--- interactive.js
+++
@@ -1,8 +0,0 @@
-var count = 0;
-var clickBtn = document.getElementById("clickBtn");
-var countDisplay = document.getElementById("count");
-
-clickBtn.onclick = function() {
-  count++;
-  countDisplay.innerHTML = count;
-};
